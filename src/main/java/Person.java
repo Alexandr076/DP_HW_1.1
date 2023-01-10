597c34c522edf7636e68c10da1db60d20b705f18
Aleksandr Zhdankov <alexandr0751@yandex.ru>
2023-01-10 23:31:30 +0300

    init

---
+++ src/main/java/Person.java
@@ -0,0 +1,24 @@
+public class Person {
+    private String firstName;
+    private String lastName;
+    private int age;
+    private String address;
+
+    public int getAge() {
+        return age;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+
+}
